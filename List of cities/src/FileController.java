2b81e992b7d18d1190933c7cdf372c42c05c10b1
jedras55 <jedrzej.ostrowski@gmail.com>
2016-07-10 14:02:16 +0200

    Added comments

--- List of cities/src/FileController.java
+++ List of cities/src/FileController.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 
 public class FileController {
 
-	private final String FILE_ALL_CITIES_PATH = "./res/allCities.txt";
-	private final String FILE_COUNTRY_CITIES_PATH = "./res/countryCities.txt";
-	private String countryCode;
+	private final String FILE_ALL_CITIES_PATH = "./res/allCities.txt"; // Path to file with all cities from http://openweathermap.org/help/city_list.txt
+	private final String FILE_COUNTRY_CITIES_PATH = "./res/countryCities.txt"; // Path to new file with result of program
+	private String countryCode; // Code of country we want to get in result for example GB for Great Britain
 	private ArrayList<String> countryCities;
 
 	public void setCountryCode(String countryCode){
@@ -30,6 +30,8 @@ public class FileController {
 			while((line1 = reader.readLine()) != null){
 				String tmp = reader.readLine();
 				if(tmp.substring(tmp.length()-2, tmp.length()).matches(countryCode)){
+					// Getting 2 last characters of each line - it's country code.
+					// If it's country code, which we wanted, program is saving all line to ArrayList
 					countryCities.add(tmp);
 				}
 			}
@@ -46,12 +48,13 @@ public class FileController {
 	}
 
 	public void saveCountryCities(ArrayList<String> countryCities){
+		// It's saving our ArrayList to new file
 		File fileCountryCitiesList = new File(FILE_COUNTRY_CITIES_PATH);
 		try(
 				BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_COUNTRY_CITIES_PATH, true));
 		) {
 			PrintWriter printwriter = new PrintWriter(fileCountryCitiesList);
-			printwriter.write("");
+			printwriter.write(""); // Cleaning file before each saving
 			printwriter.close();
 			for(String tmp: countryCities){
 				writer.write(tmp);
